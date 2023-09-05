c25370c9a12ecd7ea60fadf0fe7f7241a8657909
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-05 09:06:02 -0300

    TERMINADO

--- navigation/src/components/Menu.js
+++ navigation/src/components/Menu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome"; // Importa el icono que desees
 
 import appStyles from "../../styles.js";
 
@@ -8,21 +9,25 @@ const Menu = () => {
   const navigation = useNavigation(); 
 
   return (
-        <View style={appStyles.menuContainer}>
-    
-          <TouchableOpacity onPress={() => navigation.navigate("Screen1")}>
-            <Text style={appStyles.botonMenu1}>1</Text>
-          </TouchableOpacity>
-      
-          <TouchableOpacity onPress={() => navigation.navigate("Screen2")}>
-            <Text style={appStyles.botonMenu2}>2</Text>
-          </TouchableOpacity>
+    <View style={appStyles.menuContainer}>
+      <TouchableOpacity onPress={() => navigation.navigate("Screen1")}>
+      <Text>
+        <Icon name="book" style={appStyles.botonMenu1} /> 
+        </Text>
+      </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => navigation.navigate("Screen3")}>
-            <Text style={appStyles.botonMenu3}>3</Text>
-          </TouchableOpacity>
-        
-      </View>
+      <TouchableOpacity onPress={() => navigation.navigate("Screen2")}>
+        <Text>
+        <Icon name="user" style={appStyles.botonMenu2} /> 
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate("Screen3")}>
+      <Text>
+        <Icon name="search" style={appStyles.botonMenu3} /> 
+        </Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
